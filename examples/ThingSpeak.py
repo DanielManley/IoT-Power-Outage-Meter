1d46405a57528d1c55afd955cb0d873f8a201244
Daniel <danielemanley@gmail.com>
2020-08-05 21:03:27 +0000

    just doing things

--- examples/ThingSpeak.py
+++ examples/ThingSpeak.py
@@ -6,5 +6,5 @@ client.connect("mqtt.thingspeak.com",1883,60)
 channelId = "1112212"         # Put your channel ID here,i.e.. the number from the URL, https://thingspeak.com/channels/285697
 apiKey = "AZP2G6XY3CKLAYFC"  # Put the API key here (the Write API Key from the API Keys tab in ThingSpeak)
 
-client.publish("channels/%s/publish/%s" % (channelId,apiKey), "field1=26&field2=1013")
+client.publish("channels/%s/publish/%s" % (channelId,apiKey), "field1=50&field2=90")
 client.loop(2)
