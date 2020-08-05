ec8d47220e56418b7c165e94493abbe7fb029aca
DanielManley <36287636+DanielManley@users.noreply.github.com>
2020-08-05 13:07:43 -0800

    Update VoltageSensor.py

--- examples/VoltageSensor.py
+++ examples/VoltageSensor.py
@@ -7,27 +7,21 @@ import sys
 if not os.getegid() == 0:
     sys.exit('Script must be run as root')
 
+import paho.mqtt.client as mqtt
 import time
 from pyA20.gpio import gpio
 from pyA20.gpio import connector
 from pyA20.gpio import port
 
-GVEA = port.PA12
-#led = connector.gpio0p0     # This is the same as port.PH2
-#button = connector.gpio3p40
+client = mqtt.Client()
+client.connect("mqtt.thingspeak.com",1883,60)
+channelId = "1112212"  # Put your channel ID here
+apiKey = "AZP2G6XY3CKLAYFC"  # Put the API write key here
 
 
+GVEA = port.PA12 #set pin 12 to GVEA
 gpio.init() #initiate GPIO usage
-
-
-gpio.setcfg(GVEA,gpio.INPUT)
-"""Set directions"""
-#gpio.setcfg(led, gpio.OUTPUT)
-#gpio.setcfg(button, gpio.INPUT)
-
-"""Enable pullup resistor"""
-#gpio.pullup(GVEA, gpio.PULLUP)
-#gpio.pullup(button, gpio.PULLDOWN)     # Optionally you can use pull-down resistor
+gpio.setcfg(GVEA,gpio.INPUT) #set GVEA to input
 
 try:
     print ("Press CTRL+C to exit")
@@ -35,8 +29,8 @@ try:
         state = gpio.input(GVEA)      # Read button state
         print (state)
 	time.sleep(2)
-        """Since we use pull-up the logic will be inverted"""
-        #gpio.output(led, not state)
+	client.publish("channels/%s/publish/%s" % (channelId,apiKey), "field1="state)
+
 
 except KeyboardInterrupt:
     print ("Goodbye.")
